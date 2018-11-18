f62fc32f401a5ad736e324f53470bebfd689a3c9
Hiroki Shibahara <shibomb@gmail.com>
2018-11-18 21:33:14 +0900

    init

---
+++ src/main/java/xyz/shibomb/myfirstspigotplugin/CommandHello.java
@@ -0,0 +1,30 @@
+package xyz.shibomb.myfirstspigotplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandHello implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        System.out.println(sender);
+        System.out.println(cmd);
+        System.out.println(label);
+        System.out.println(args);
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (label.compareToIgnoreCase("hello") == 0) {
+                sender.sendMessage("Hello!! " + player.getDisplayName());
+            }
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+
+}
---
+++ src/main/java/xyz/shibomb/myfirstspigotplugin/MyFirstSpigotPlugin.java
@@ -0,0 +1,22 @@
+package xyz.shibomb.myfirstspigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyFirstSpigotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info(this.getName() + "#onEnable()");
+
+        // Plugin startup logic
+
+        this.getCommand("hello").setExecutor(new CommandHello());
+        this.getCommand("seeyou").setExecutor(new CommandSeeyou());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(this.getName() + "#onDisable()");
+        // Plugin shutdown logic
+    }
+}
